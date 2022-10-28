bbc0ebe0f8235198ebbc3228a8d1a178cf245929
USER <USER@DESKTOP-JGK4IQ5>
2022-10-28 18:06:35 +0600

    first commit

---
+++ roopProject/roopWebParent/roopBackEnd/src/main/java/com/roop/admin/RoopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.roop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoopBackEndApplication.class, args);
+	}
+
+}
